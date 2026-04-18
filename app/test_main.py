ba93930cbe867ec23cb63d07eea04552c62abb48
Dmytro Herman <dmytroherman@tutamail.com>
2026-04-18 14:43:53 +0000

    Update tests for edge cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pytest
 from app.main import get_human_age
 
@@ -5,7 +6,6 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        # Examples from README
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -14,16 +14,12 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
-        # Edge cases: Boundaries (Check value doesn't change too early)
-        (14, 14, [0, 0]),
-        (23, 23, [1, 1]),
-        # Edge cases: Negative and Very Large Numbers
-        (-1, -5, [0, 0]),
+        (-1, -1, [0, 0]),
         (1000, 1000, [246, 197]),
     ]
 )
-def test_get_human_age_values(cat_age: int,
-                              dog_age: int, expected: list[int]) -> None:
+def test_get_human_age_values(cat_age: int, dog_age: int,
+                              expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
@@ -36,6 +32,6 @@ def test_get_human_age_values(cat_age: int,
         (None, 15),
     ]
 )
-def test_get_human_age_wrong_types(cat_age: any, dog_age: any) -> None:
+def test_get_human_age_wrong_types(cat_age: Any, dog_age: Any) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
